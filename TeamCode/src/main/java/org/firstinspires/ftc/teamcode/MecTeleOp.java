0e4c3646f0854006a85fe9f6dc4a58721602bf76
Arnav Kumar <arnavpk3@gmail.com>
2022-09-19 18:12:29 -0500

    MecTeleOp Added

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecTeleOp.java
@@ -0,0 +1,73 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "MecTeleOp", group = "TeleOp")
+public class MecTeleOp extends OpMode {
+    // wheels make a diamond shape looking from above
+    DcMotor fl;
+    DcMotor fr;
+    DcMotor bl;
+    DcMotor br;
+
+    @Override
+    public void init() {
+        fl = hardwareMap.dcMotor.get("fl");
+        fr = hardwareMap.dcMotor.get("fr");
+        bl = hardwareMap.dcMotor.get("bl");
+        br = hardwareMap.dcMotor.get("br");
+        fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        br.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        fr.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        bl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        br.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        fr.setDirection(DcMotorSimple.Direction.REVERSE);
+        br.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    @Override
+    public void loop() {
+        if(Math.abs(gamepad1.left_stick_x) > 0.1 || Math.abs(gamepad1.left_stick_y) > 0.1 || Math.abs(gamepad1.right_stick_x) > 0.1) {
+            double lx = gamepad1.left_stick_x;
+            double ly = -gamepad1.left_stick_y;
+            double rx = -gamepad1.right_stick_x;
+            double denominator = Math.max(Math.abs(lx) + Math.abs(ly) + Math.abs(rx), 1);
+            double FLP = (ly + lx + rx) / denominator;
+            double FRP = (ly - lx - rx) / denominator;
+            double BLP = (ly - lx + rx) / denominator;
+            double BRP = (ly + lx - rx) / denominator;
+            if(gamepad1.right_trigger > 0.1){
+                fl.setPower(FLP * .35);
+                fr.setPower(FRP * .35);
+                bl.setPower(BLP * .35);
+                br.setPower(BRP * .35);
+            } else {
+                fl.setPower(FLP);
+                fr.setPower(FRP);
+                bl.setPower(BLP);
+                br.setPower(BRP);
+            }
+        }
+        else{
+            fl.setPower(0);
+            fr.setPower(0);
+            bl.setPower(0);
+            br.setPower(0);
+        }
+
+    }
+}
+
+
+
