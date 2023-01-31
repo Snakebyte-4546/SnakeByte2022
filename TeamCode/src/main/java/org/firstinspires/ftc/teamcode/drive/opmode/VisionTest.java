3170dc0bffc507638e63e2f54b3c85628c5cc591
Noah Blaut <noah.blaut@outlook.com>
2023-01-31 14:45:19 -0600

    Cone Automation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/VisionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/VisionTest.java
@@ -2,42 +2,19 @@ package org.firstinspires.ftc.teamcode.drive.opmode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.util.robot.ConeAutomation;
-import org.openftc.easyopencv.OpenCvCamera;
-import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.firstinspires.ftc.teamcode.util.robot.ConeAutomationPipeline;
 
-@Autonomous(name="Auto: SkyStone Detector")
+@Autonomous(name="Vision Test")
 public class VisionTest extends LinearOpMode {
-    int width = 320;
-    int height = 240;
-    ConeAutomation detector = new ConeAutomation(true);
-    OpenCvCamera camera;
+    ConeAutomation ConeAutomation = new ConeAutomation();
+    ConeAutomationPipeline pipeline = new ConeAutomationPipeline(true);
 
     @Override
     public void runOpMode() {
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
-        camera.setPipeline(detector);
-        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
-            @Override
-            public void onOpened() {
-                camera.startStreaming(width, height, OpenCvCameraRotation.UPRIGHT);
-            }
-
-            @Override
-            public void onError(int errorCode) {
-                telemetry.addData("Camera Error:", errorCode);
-                telemetry.update();
-            }
-        });
         waitForStart();
-        while (!isStopRequested()){
-            telemetry.addData("Area:", detector.getArea());
-            telemetry.addData("Center x:", detector.getCenterX());
-            telemetry.addData("Center y:", detector.getCenterY());
+        while (opModeIsActive() & !isStopRequested()) {
+            telemetry.addData("Center of Cone: ", ConeAutomation.getConeCenter(pipeline));
             telemetry.update();
         }
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/robot/ConeAutomation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/robot/ConeAutomation.java
@@ -1,96 +1,37 @@
 package org.firstinspires.ftc.teamcode.util.robot;
-import java.util.ArrayList;
-import java.util.List;
 
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfPoint;
-import org.opencv.core.MatOfPoint2f;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.opencv.core.Point;
-import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
-import org.openftc.easyopencv.OpenCvPipeline;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
 
-public class ConeAutomation extends OpenCvPipeline {
-    private boolean isRed;
-    private double area;
-    private Scalar lowHSV;
-    private Scalar highHSV;
-    private final int threshold = 100;
-    private Rect[] boundRect;
-    private int highPos = 0;
-    private int cx = 0;
-    private int cy = 0;
+public class ConeAutomation {
+    OpenCvCamera camera;
+    ConeAutomationPipeline ConeAutomationPipeline;
 
-    public ConeAutomation(boolean isRed) {
-        this.isRed = isRed;
-    }
+    public ConeAutomationPipeline cameraSetup(LinearOpMode opMode) {
+        int cameraMonitorViewId = opMode.hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", opMode.hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(opMode.hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        ConeAutomationPipeline = new ConeAutomationPipeline(true);
 
-    @Override
-    public Mat processFrame(Mat input) {
-        Mat mat = new Mat();
-        Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGB2HSV);
-
-        //Set Red / Blue Threshold
-        if (isRed){
-            lowHSV = new Scalar(0, 155,120);
-            highHSV = new Scalar(90, 255, 255);
-        } else {
-            lowHSV = new Scalar(31, 51,121);
-            highHSV = new Scalar(128, 255, 255);
-        }
-        Mat thresh = new Mat();
-
-        //b&w image where white represents cone.
-        Core.inRange(mat, lowHSV, highHSV, thresh);
-
-        // findContours connects the edges of the thresh.
-        // We then find the bounding rectangles of those contours
-        List<MatOfPoint> contours = new ArrayList<>();
-        Mat hierarchy = new Mat();
-        Imgproc.findContours(thresh, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
-
-        //Get Boundary Boxes
-        MatOfPoint2f[] contoursPoly = new MatOfPoint2f[contours.size()];
-        Rect[] boundRect = new Rect[contours.size()];
-        for (int i = 0; i < contours.size(); i++) {
-            contoursPoly[i] = new MatOfPoint2f();
-            Imgproc.approxPolyDP(new MatOfPoint2f(contours.get(i).toArray()), contoursPoly[i], 3, true);
-            boundRect[i] = Imgproc.boundingRect(new MatOfPoint(contoursPoly[i].toArray()));
-        }
-
-        //Iterate through the bounding boxes and draw the box with the most area (theoretically the cone)
-        //Catch the possibility of there being no bounding boxes
-        try {
-            double highArea = boundRect[0].area();
-            int highPos = 0;
-            for (int i = 0; i != boundRect.length; i++) {
-                if (highArea < boundRect[i].area()) {
-                    highArea = boundRect[i].area();
-                    highPos = i;
-                }
+        camera.setPipeline(ConeAutomationPipeline);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                camera.startStreaming(640, 480, OpenCvCameraRotation.UPRIGHT);
             }
-            Imgproc.rectangle(mat, boundRect[highPos], new Scalar(250, 250, 250), 3);
-            area = boundRect[highPos].area();
-            cx = boundRect[highPos].x + boundRect[highPos].width / 2;
-            cy = boundRect[highPos].y + boundRect[highPos].height / 2;
-        }
-        catch(ArrayIndexOutOfBoundsException exception) {
-            //No Cone Found
-        }
-        return mat;
-    }
-    public double getArea() {
-        return area;
-    }
 
-    public double getCenterX() {
-        return cx;
+            @Override
+            public void onError(int errorCode) {
+                opMode.telemetry.addData("Camera Error:", errorCode);
+                opMode.telemetry.update();
+            }
+        });
+        return ConeAutomationPipeline;
     }
-
-    public double getCenterY() {
-        return cy;
+    public Point getConeCenter(ConeAutomationPipeline pipeline) {
+        return pipeline.center;
     }
-
 }
